ea78892da191b74aefff2681154f0158babc673b
Vita <ekokalgan@gmail.com>
2023-02-17 11:02:51 +0200

    from revision-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,34 +9,38 @@ form.addEventListener('input', dataLocalStorage);
 form.addEventListener('submit', throttle(onFormSubmit, 500));
 
 //створили об'єкт який буде зберігати введенні значення
-const formData = {};  
+let formData = {};  
 
 //Вішаємо спрослуховування лише на форму
 form.addEventListener('input', event => {
 
   formData[event.target.name] = event.target.value; //отримуэмо введені значення
-
+ // console.log(formData);
   localStorage.setItem('feedback-form-state', JSON.stringify(formData)); //записуємо значення в localStorage
   
-});console.log(formData);
+});
   
 // відправка форми
 function onFormSubmit(event){
   event.preventDefault(); //забороняємо перезавантаження сторінки за замовчуванням
-  
+  console.log(JSON.parse(localStorage.getItem(STOREGE_KEY)));
+ 
   const formReset = event.currentTarget;
   formReset.reset(); //очищуємо введені значення в формі 
 
   localStorage.removeItem(STOREGE_KEY); //видаляэмо введені значення в localStorage якщо виконана відправка форми 
+  
+  formData = {};
+}; //console.log(formData);
 
-}; 
 
 function dataLocalStorage() {
-   const savedData = JSON.parse(localStorage.getItem(STOREGE_KEY));
+   const savedData = JSON.parse(localStorage.getItem(STOREGE_KEY));//отримуємо значення з сховища localStorage
    
-     if (savedData) {
-      console.log(savedData);
-    } 
+     if (savedData) {             //перевірка на наявність STOREGE_KEY в localStorage
+        form.value = savedData;
+        //console.log(form.value);
+          } 
   }; 
 
 
